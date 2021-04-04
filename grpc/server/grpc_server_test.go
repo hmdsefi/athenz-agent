09eac1b0e753a270b5e5f5301aa1ba3b246a0e4e
Hamed Yousefi <hdyousefi@gmail.com>
2021-04-05 04:24:04 +0430

    Setup mutual TLS on gRPC server (#13)
    * edit readme
    
    * Mutual TLS added to gRPC server server options

---
+++ grpc/server/grpc_server_test.go
@@ -0,0 +1,17 @@
+/**
+ * Copyright © 2019 Hamed Yousefi <hdyousefi@gmail.com.com>.
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file.
+ *
+ * Created by IntelliJ IDEA.
+ * User: Hamed Yousefi
+ * Email: hdyousefi@gmail.com
+ * Date: 4/5/21
+ * Time: 2:22 AM
+ *
+ * Description:
+ *
+ */
+
+package server
